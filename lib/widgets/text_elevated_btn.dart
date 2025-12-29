211c238f3e489464d8c0c0edc2705b9e9cde68fc
chirag-mokaritya <mokariyachirag10@gmail.com>
2025-12-29 19:51:26 +0530

    Buttons

--- lib/widgets/text_elevated_btn.dart
+++ lib/widgets/text_elevated_btn.dart
@@ -7,6 +7,17 @@ class ButtonsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Text btn & Elevated btn")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blueAccent,
+        foregroundColor: Colors.white,
+        elevation: 10,
+        //shadow
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+        tooltip: "take photo",
+        //show in long press or hover
+        child: Icon(Icons.add_a_photo_sharp),
+      ),
       body: Container(
         padding: EdgeInsets.all(20),
         child: Column(
@@ -37,6 +48,13 @@ class ButtonsWidget extends StatelessWidget {
                 foregroundColor: Colors.white, //text color
               ),
               child: Text('Submit'),
+            ),
+            SizedBox(height: 20),
+            IconButton(
+              onPressed: () {},
+              color: Colors.blueAccent,
+              iconSize: 40,
+              icon: Icon(Icons.calendar_month),
             ),
           ],
         ),
