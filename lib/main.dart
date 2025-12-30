32822a2d64a57ce1657cbb303fe08ea36f948534
chirag-mokaritya <mokariyachirag10@gmail.com>
2025-12-30 19:45:00 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/widgets/alert_dialogbox.dart';
 import 'package:flutter_basic/widgets/col_widget.dart';
 import 'package:flutter_basic/widgets/container_widget.dart';
+import 'package:flutter_basic/widgets/dismissible.dart';
+import 'package:flutter_basic/widgets/drawer_widget.dart';
+import 'package:flutter_basic/widgets/gridview.dart';
+import 'package:flutter_basic/widgets/image_widget.dart';
+import 'package:flutter_basic/widgets/listview.dart';
+import 'package:flutter_basic/widgets/listview_builder.dart';
 import 'package:flutter_basic/widgets/row_widget.dart';
 import 'package:flutter_basic/widgets/sizedbox_widget.dart';
+import 'package:flutter_basic/widgets/snackbar_widget.dart';
+import 'package:flutter_basic/widgets/text_elevated_btn.dart';
 import 'package:flutter_basic/widgets/wrap_widget.dart';
 
 void main() {
@@ -23,7 +32,16 @@ class MyApp extends StatelessWidget {
       // home: SizedBoxWidget(),
       // home: RowWidget(),
       // home: ColWidget(),
-      home: WrapWidget(),
+      // home: WrapWidget(),
+      // home: ButtonsWidget(),
+      // home: Listview(),
+      // home: ListviewBuilder(),
+      // home: Gridview(),
+      // home: SnackbarWidget(),
+      // home:DismissibleWidget(),
+      // home: DrawerWidget(),
+      // home:ImageWidget(),
+      home: AlertDialogWidget(),
     );
   }
 }
